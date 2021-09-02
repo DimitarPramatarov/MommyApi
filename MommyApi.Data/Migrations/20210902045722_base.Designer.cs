6ea6b08c01bc89005761174b5903afda9942724f
Dimitar Pramatarov <dim.pramatarov@gmail.com>
2021-09-02 07:58:18 +0300

    Db migrate and updae

--- MommyApi.Data/Migrations/20210902045722_base.Designer.cs
+++ MommyApi.Data/Migrations/20210902045722_base.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MommyApi.Data;
 
 namespace MommyApi.Data.Migrations
 {
     [DbContext(typeof(MommyApiDbContext))]
-    partial class MommyApiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210902045722_base")]
+    partial class @base
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MommyApi.Data/Migrations/20210902045722_base.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MommyApi.Data.Migrations
+{
+    public partial class @base : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Text",
+                table: "Answers");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Votes",
+                table: "SubAnswers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Posts",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Votes",
+                table: "Posts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Answers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Votes",
+                table: "Answers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Votes",
+                table: "SubAnswers");
+
+            migrationBuilder.DropColumn(
+                name: "Votes",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Answers");
+
+            migrationBuilder.DropColumn(
+                name: "Votes",
+                table: "Answers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Posts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Text",
+                table: "Answers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
